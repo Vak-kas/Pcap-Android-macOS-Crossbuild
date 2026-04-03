3e3a8ac54d92738ad0af4332c51b431b8f1b3b2b
서민재 <hey_minj@naver.com>
2026-04-03 20:00:38 +0900

    delete : daemon없이 수행되던 nic list 가져오기 함수 제거

--- pcapmanager.h
+++ pcapmanager.h
@@ -4,16 +4,11 @@
 #include <QString>
 #include <QList>
 
-struct NICInfo
-{
-    QString name;        // 실제 NIC (pcap에서 사용)
-    QString description; // UI 표시용
-};
 
 class PcapManager
 {
 public:
-    static QList<NICInfo> getNICList();
+
 };
 
 
