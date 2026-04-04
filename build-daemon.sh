904f198b586298cbaae18a8f31ef35e90a54ba44
서민재 <hey_minj@naver.com>
2026-04-04 19:44:31 +0900

    fix : mjDaemon의 기능확장으로 인한 링킹 제대로 설정

--- build-daemon.sh
+++ build-daemon.sh
@@ -1,1 +1,17 @@
-~/Library/Android/sdk/ndk/26.1.10909125/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang++ ./root/mjDaemon.cpp -o mjDaemon -static-libstdc++
+#!/bin/bash
+NDK_BIN=~/Library/Android/sdk/ndk/26.1.10909125/toolchains/llvm/prebuilt/darwin-x86_64/bin
+SYSROOT=$NDK_BIN/../../sysroot
+
+$NDK_BIN/aarch64-linux-android28-clang++ \
+    -fPIE -pie \
+    ./root/mjDaemon.cpp \
+    ./root/handler.cpp \
+    ./pcapmanager.cpp \
+    -I../libpcap/out/android/include \
+    -L../libpcap/out/android/lib \
+    -lpcap \
+    -o mjDaemon \
+    -static-libstdc++ \
+    -O2
+
+echo "[Daemon build done]"
