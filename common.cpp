45b5c027778b97d70ff39fccf4eaa7948756f2ee
서민재 <hey_minj@naver.com>
2026-04-10 16:08:40 +0900

    feat : pcap 프로세스 관리용 전역변수 생성

--- common.cpp
+++ common.cpp
@@ -1,1 +1,4 @@
 #include "common.h"
+
+bool g_running = false;
+pcap_t* g_pcap = nullptr;
--- common.h
+++ common.h
@@ -1,4 +1,7 @@
+#pragma once
 #include <stdlib.h>
+#include <pcap.h>
+
 struct MsgHeader
 {
     uint32_t type;
@@ -9,3 +12,8 @@ struct MsgHeader
 #define MSG_PKT  2
 #define MSG_LOG  3
 #define MSG_ERR  4
+
+
+extern pcap_t* g_pcap;
+extern bool g_running;
+extern pid_t g_child_pid;
