608edf105ae3c5203bb7a762eea540110e68648d
서민재 <hey_minj@naver.com>
2026-04-10 16:11:49 +0900

    feat : START_CAPTURE 명령어 입력시 패킷 처리하는 메인 함수 구현

--- root/mjDaemon.cpp
+++ root/mjDaemon.cpp
@@ -4,17 +4,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include "handler.h"
+#include "../common.h"
+#include "../pcapmanager.h"
 
 char cmd[256];
+pid_t g_child_pid = -1;
 
 int main()
 {
-    while (fgets(cmd, sizeof(cmd), stdin)) 
+    while (1)
     {
-        if (strncmp(cmd, "GET_NIC", 7) == 0) {
-            handleGetNIC();
+        // 1. 명령 처리
+        if (fgets(cmd, sizeof(cmd), stdin))
+        {
+            if (strncmp(cmd, "GET_NIC", 7) == 0)
+            {
+                handleGetNIC();
+            }
+            else if (strncmp(cmd, "START_CAPTURE", 13) == 0)
+            {
+                char* nic = cmd + 14;
+                nic[strcspn(nic, "\n")] = 0; // 개행 제거
+                handleStartCapture(nic);
+            }
+            else if (strncmp(cmd, "STOP_CAPTURE", 12) == 0)
+            {
+                handleStopCapture();
+            }
+            else if (strncmp(cmd, "EXIT", 4) == 0)
+            {
+                handleStopCapture();
+                exit(0);
+            }
         }
-        usleep(100000);
+            usleep(10000); // 10ms
     }
-    return 0;
 }
