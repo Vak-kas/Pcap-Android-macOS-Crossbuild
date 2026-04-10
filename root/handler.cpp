1a10a5c013eb9f94cbd11d0178f765cee662f63d
서민재 <hey_minj@naver.com>
2026-04-10 16:13:16 +0900

    feat : packet 캡처 시작, 종료 함수 코드 작성 및 임시로 데이터 전송 코드 작성

--- root/handler.cpp
+++ root/handler.cpp
@@ -1,9 +1,4 @@
 #include "handler.h"
-#include <pcap.h>
-#include <stdio.h>
-#include "../common.h"
-#include <string.h>
-#include "../pcapmanager.h"
 
 void handleGetNIC()
 {
@@ -24,4 +19,73 @@ void sendNIC(const char* name)
     fwrite(&header, sizeof(header), 1, stdout);
     fwrite(name, header.length, 1, stdout);
     fflush(stdout);
+}
+
+void handleStartCapture(const char* nicName)
+{
+    if (g_running)
+    {
+        return;
+    }
+
+    g_pcap = initPcap(nicName);
+    if (!g_pcap) 
+    {
+        return;
+    }
+    g_running = true;
+
+    g_child_pid = fork();
+
+    if (g_child_pid == 0)
+    {
+        pcap_loop(g_pcap, -1, packetHandler, NULL);
+        exit(0);
+    }
+    else if (g_child_pid < 0)
+    {
+        perror("fork failed");
+        g_running = false;
+    }
+    else
+    {
+        // parent는 그냥 리턴 (명령 계속 받음)
+    }
+}
+
+
+void handleStopCapture()
+{
+    if (g_running && g_child_pid > 0)
+    {
+        kill(g_child_pid, SIGTERM);
+        waitpid(g_child_pid, NULL, 0);
+
+        g_child_pid = -1;
+    }
+
+    if (g_pcap)
+    {
+        closePcap(g_pcap);
+        g_pcap = nullptr;
+    }
+
+    g_running = false;
+}
+
+void handlePacket(const struct pcap_pkthdr* header, const u_char* packet)
+{
+    MsgHeader msgHeader;
+    msgHeader.type = MSG_PKT;
+    msgHeader.length = header->caplen;
+
+    //원본 패킷 그대로 전송(임시)
+    fwrite(&msgHeader, sizeof(msgHeader), 1, stdout);
+    fwrite(packet, header->caplen, 1, stdout);
+    fflush(stdout);
+}
+
+void packetHandler(u_char* user, const struct pcap_pkthdr* header, const u_char* packet)
+{
+    handlePacket(header, packet);
 }
--- root/handler.h
+++ root/handler.h
@@ -1,2 +1,17 @@
+#include <pcap.h>
+#include <stdio.h>
+#include "../common.h"
+#include <string.h>
+#include "../pcapmanager.h"
+#include <unistd.h>   // fork()
+#include <sys/types.h> // pid_t
+#include <signal.h>   // kill()
+#include <sys/wait.h> // waitpid()
+
+
 void handleGetNIC();
 void sendNIC(const char* name);
+void handleStartCapture(const char* nicName);
+void handleStopCapture();
+void handlePacket(const struct pcap_pkthdr* header, const u_char* packet);
+void packetHandler(u_char* user, const struct pcap_pkthdr* header, const u_char* packet);
